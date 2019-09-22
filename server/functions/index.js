3630b6292c93e186c671018d3e60e7b500a2290f
rramsisa <rishabhramsisaria25@gmail.com>
2019-09-22 16:50:33 -0400

    JS functions

---
+++ server/functions/index.js
@@ -0,0 +1,40 @@
+const functions = require('firebase-functions');
+const cors = require('cors')({
+    origin: true
+});
+const admin = require('firebase-admin');
+admin.initializeApp();
+const database = admin.database().ref('/items');
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+    res.send("Hello from a Serverless Database!");
+});
+
+exports.addItem = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        if (req.method !== 'POST') {
+            return res.status(401).json({
+                message: 'Not allowed'
+            })
+        }
+        console.log(req.body)
+        const item = req.body.item
+        database.push({
+            item
+        });
+        let items = [];
+        return database.on('value', (snapshot) => {
+            snapshot.forEach((item) => {
+                items.push({
+                    id: item.key,
+                    items: item.val().item
+                });
+            });
+            res.status(200).json(items)
+        }, (error) => {
+            res.status(error.code).json({
+                message: `Something went wrong. ${error.message}`
+            })
+        })
+    })
+})
