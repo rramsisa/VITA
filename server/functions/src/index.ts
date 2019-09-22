b4f91e9e897002fbeea8e98a54af4a2f6ed7c85d
rramsisa <rishabhramsisaria25@gmail.com>
2019-09-22 15:52:02 -0400

    created firebase server

---
+++ server/functions/src/index.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+admin.initializeApp(functions.config().firebase);
+
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+
+export const webApi = functions.https.onRequest(main);
+
+app.get('/warmup', (request, response) => {
+	response.send('Warming up friend.');
+});
