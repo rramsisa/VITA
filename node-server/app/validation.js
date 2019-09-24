17a16c6ebf0e4b15266c5535ecc11373a410a7d4
niralirai <niralirai@yahoo.com>
2019-09-23 20:52:06 -0400

    change password - fixing

--- node-server/app/validation.js
+++ node-server/app/validation.js
@@ -33,5 +33,24 @@ const loginValidation = (data) => {
     return Joi.validate(data, schema);
 };
 
+const changePasswordValidation = (data) => {
+    const schema = {
+        email: Joi.string()
+            .min(6)
+            .required()
+            .email(),
+        password: Joi.string()
+            .min(6)
+            .required(),
+        newPassword: Joi.string()
+            .min(6)
+            .required()
+    };
+
+    return Joi.validate(data, schema);
+};
+
+
 module.exports.registerValidation = registerValidation;
 module.exports.loginValidation = loginValidation;
+module.exports.changePasswordValidation = changePasswordValidation;
