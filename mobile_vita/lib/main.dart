260de003267c9a19d674e0c987df1f3057fe9ab3
Cameron Glass <glass10@purdue.edu>
2019-09-30 00:38:43 -0400

    Debug Banner Removed

--- mobile_vita/lib/main.dart
+++ mobile_vita/lib/main.dart
@@ -14,6 +14,7 @@ class LoginApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'VITA Login',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
